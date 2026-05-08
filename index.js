09c04376fe39848fb00e8bd6010fd4618924bf7a
smartmethodowner904 <quotexrealid9045@gmail.com>
2026-05-08 17:24:04 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Telegraf, Markup } = require("telegraf");
 
-const bot = new Telegraf("8585925975:AAG4go4UJMTWf61Qh8c0Y3LtoPzpAcMOUZI");
+const bot = new Telegraf("8585925975:AAE5Vx3BdRFbEog1i5Nl1m288Pewude8-Ro");
 
 /* ================= GROUP LIST ================= */
 
